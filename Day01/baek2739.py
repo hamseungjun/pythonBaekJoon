d5665b0bba5e0f5f08e04c4d7f35e5ab08726476
hamseungjun <hamseungjun1029@gmail.com>
2024-03-05 20:39:24 +0900

    day01

---
+++ Day01/baek2739.py
@@ -0,0 +1,5 @@
+n = int(input())
+
+for i in range(1, 10):
+    s ="{0} * {1} = {2}".format(n, i, n*i)
+    print(s)
---
+++ Day01/beak1157.py
@@ -0,0 +1,23 @@
+# str = input().upper()
+# str_list = list(set(str))
+# cnt = []
+
+# for i in str_list:
+#     count = str.count(i)
+#     cnt.append(count)
+
+# if cnt.count(max(cnt)) > 1:
+#     print("?")
+# else:
+#     print(str_list[(cnt.index(max(cnt)))])
+str = input().upper()
+str_list = list(set(str))
+cnt = []
+
+for i in str_list:
+    count = str.count(i)
+    cnt.append(count)
+if cnt.count(max(cnt) > 2):
+    print("?")
+else:
+    print(str_list[cnt.index(max(cnt))])
